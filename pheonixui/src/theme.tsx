1cb6eeed2e14236235bf5e87256839d032c52f13
PheonixDevs <39148459+PheonixDevs@users.noreply.github.com>
2024-11-07 11:38:34 +0530

    PT189:Button

---
+++ pheonixui/src/theme.tsx
@@ -0,0 +1,33 @@
+const themestyle = {
+    colors: {
+      primary: "#181D8C",
+      disabled:"#bfbfbf",
+      secondary: "#DE0093",
+      fontfamily: "Poppins",
+      error:"#D32F2F",
+      white:"#FFFFFF",
+    },
+    fontSizes: {
+      xsmall: "16px",
+      small: "18px",
+      medium: "24px",
+      large: "32px",
+      xlarge: "32px",
+      sm:"20px"
+    },
+    spacing: {
+      small: "10px",
+      medium: "20px",
+      large: "30px",
+      xlarge: "40px",
+    },
+    fontWeight: {
+      xs:400,
+      sm:700,
+      md: 900,
+      medium:500
+    },
+   
+  };
+  export default themestyle;
+  
