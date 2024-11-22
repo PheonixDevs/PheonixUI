82894f35f7b3284597b02708f80698e3bb59c39f
PheonixDevs <dev@pheonixsolutions.com>
2024-11-22 11:30:16 +0530

    PT-204:Autocomplete

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as PheonixSnackbar } from './Components/PheonixSnackbar';
 export { default as PheonixTextfield } from './Components/PheonixTextfield';
 export { default as PheonixBox } from './Components/PheonixBox';
 export {default as PheonixPaper} from './Components/PheonixPaper';
+export {default as PheonixAutocomplete} from './Components/PheonixAutocomplete';
