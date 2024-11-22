698628058db0740a38b58e118a33afdf24cd1bf5
PheonixDevs <39148459+PheonixDevs@users.noreply.github.com>
2024-11-22 10:17:58 +0530

    Revert "Autocomplete Component"

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as PheonixButton } from './Components/PheonixButton';
-export { default as PheonixSnackbar } from './Components/PheonixSnackbar';
-export { default as PheonixTextfield } from './Components/PheonixTextfield';
-export { default as PheonixBox } from './Components/PheonixBox';
-export {default as PheonixPaper} from './Components/PheonixPaper';
-export {default as PhoenixAutocomplete} from './Components/PheonixAutocomplete';
