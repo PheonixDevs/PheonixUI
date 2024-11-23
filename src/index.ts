0e748442b8d839c701d526488ec6959451681f16
PheonixDevs <39148459+PheonixDevs@users.noreply.github.com>
2024-11-23 09:18:52 +0530

    Revert "Feature/pt 204 autocomplete"

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as PheonixButton } from './Components/PheonixButton';
-export { default as PheonixSnackbar } from './Components/PheonixSnackbar';
-export { default as PheonixTextfield } from './Components/PheonixTextfield';
-export { default as PheonixBox } from './Components/PheonixBox';
-export {default as PheonixPaper} from './Components/PheonixPaper';
-export {default as PheonixAutocomplete} from './Components/PheonixAutocomplete';
-export {default as PheonixSkeleton} from './Components/PheonixSkeleton';
-export {default as PheonixDialog} from './Components/PheonixDialog';
-export {default as PheonixTextarea} from './Components/PheonixTextarea';
