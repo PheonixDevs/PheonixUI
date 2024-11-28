132a11d5acc4366e4fdb3d9c98a29d8cecae1d8e
PheonixDevs <39148459+PheonixDevs@users.noreply.github.com>
2024-11-28 12:49:47 +0530

    Revert "sidebar"

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,3 @@ export { default as PheonixCheckbox } from "./Components/PheonixCheckbox";
 export { default as PheonixAccordian } from "./Components/PheonixAccordian";
 export { default as PheonixCard } from "./Components/PheonixCard";
 export { default as PheonixLogin } from "./Components/PheonixLogin";
-export { default as PheonixSideBar } from "./Components/PheonixSidebar"
